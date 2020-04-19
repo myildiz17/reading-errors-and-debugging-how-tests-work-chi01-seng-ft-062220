d4ce6da0d16686a7841851b09d252e48e56581f3
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-19 04:04:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 first_number=3 # Add your variables here
 second_number=-5
-sum=first_number+second_number
-difference=first_number-second_number
+sum=first_number + second_number
+difference=first_number - second_number
+product=first_number * second_number
